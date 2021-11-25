cbbb578a03c0c0e1ddf6822a61adf5ac53c6d442
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-11-25 21:19:19 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -42,7 +42,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 START_MSG = environ.get('START_MSG', default_start_msg)
 START_IMG = environ.get("START_IMG", "https://telegra.ph/file/7e7045c6d4e402131e305.jpg")
 MAIN_GROUP = environ.get("MAIN_GROUP", "")
-
+JOIN_IMG = environ.get("JOIN_IMG", "")
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/7f65f67bcb5257b7d0f35.jpg")
 RESULT_MSG = environ.get("RESULT_MSG", "")
 NO_TXT = environ.get("NO_TXT", "")
